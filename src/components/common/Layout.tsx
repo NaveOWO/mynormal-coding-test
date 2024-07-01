e1dae50f7bfab255b581853efdc690b8c8b4e32b
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2024-07-02 00:53:21 +0900

    feat: layout컴포넌트 생성

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,37 @@
+import React, { PropsWithChildren } from 'react';
+import styled from 'styled-components';
+import background from '../../assets/images/PaintedBackground.png';
+import theme from '../../styles/theme';
+
+export default function Layout({ children }: PropsWithChildren) {
+  return (
+    <Background>
+      <ContentSection>{children}</ContentSection>
+    </Background>
+  );
+}
+
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
+
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const ContentSection = styled.section`
+  width: 43rem;
+  height: 100vh;
+
+  padding: 4.1rem 1.4rem;
+
+  background-color: ${theme.colors.white};
+  overflow: scroll;
+`;
