bc69cab051108a4f989d63d552f2860d0c2edb3f
NaveOWO <87578512+NaveOWO@users.noreply.github.com>
2024-07-03 03:52:38 +0900

    refactor: zustand selector설정

--- src/hooks/zuztand/store.ts
+++ src/hooks/zuztand/store.ts
@@ -2,23 +2,28 @@ import { create } from 'zustand';
 
 interface Favorite {
   favorites: Set<string>;
-  register: (state: string) => void;
-  unRegister: (state: string) => void;
+  actions: { register: (state: string) => void; unRegister: (state: string) => void };
 }
 export const useFavoriteStore = create<Favorite>((set) => ({
   favorites: new Set(),
-  register: (state) => {
-    set((prev) => {
-      const newSet = new Set(prev.favorites);
-      newSet.add(state);
-      return { favorites: newSet };
-    });
-  },
-  unRegister: (state) => {
-    set((prev) => {
-      const newSet = new Set(prev.favorites);
-      newSet.delete(state);
-      return { favorites: newSet };
-    });
+  actions: {
+    register: (state) => {
+      set((prev) => {
+        const newSet = new Set(prev.favorites);
+        newSet.add(state);
+        return { favorites: newSet };
+      });
+    },
+    unRegister: (state) => {
+      set((prev) => {
+        const newSet = new Set(prev.favorites);
+        newSet.delete(state);
+        return { favorites: newSet };
+      });
+    },
   },
 }));
+
+export const favoriteStoreAction = (state: Favorite) => {
+  return state.actions;
+};
